20ff3bbf87ca29ee80e1131eca2c1af3e7946533
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-28 08:41:03 +0800

    Nuget pack

--- nuget_pack.ps1
+++ nuget_pack.ps1
@@ -1,2 +1,4 @@
 dotnet pack src/Extensions --configuration Release --output ../.nuget_releases/
 dotnet pack src/Extensions.AspNetCore --configuration Release --output ../.nuget_releases/
+dotnet pack src/Extensions.FluentValidation --configuration Release --output ../.nuget_releases/
+dotnet pack src/Extensions.FluentValidation.Constraints --configuration Release --output ../.nuget_releases/
