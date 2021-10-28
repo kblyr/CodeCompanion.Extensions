7cb28bb9a8b515d021e429be3dfe216e29db7898
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-28 11:26:44 +0800

    Extensions.Dapper.Postgres

--- nuget_pack.ps1
+++ nuget_pack.ps1
@@ -1,4 +1,5 @@
 dotnet pack src/Extensions --configuration Release --output ../.nuget_releases/
 dotnet pack src/Extensions.AspNetCore --configuration Release --output ../.nuget_releases/
+dotnet pack src/Extensions.Dapper.Postgres --configuration Release --output ../.nuget_releases/
 dotnet pack src/Extensions.FluentValidation --configuration Release --output ../.nuget_releases/
 dotnet pack src/Extensions.FluentValidation.Constraints --configuration Release --output ../.nuget_releases/
